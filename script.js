b01dd35d2fe93a848d069c5ab658e3c5897fd728
siaprincessm <siaprincessm@gmail.com>
2022-07-15 21:16:32 +0000

    add querySelctrbutton

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ function main() {
         let map = initMap();
         window.addEventListener("DOMContentLoaded",function(){
 
-            document.querySelector("#btnSearch").addEventListener('click',function(){
-                let query = document.querySelector("#textQuery").value;
+            document.querySelector("#btnSearch").addEventListener('click', async function(){
+                let query = document.querySelector("#txtQuery").value;
+                let center = map.getBounds().getCenter();
+                let results = await search(center.lat, center.lng, query);
+                console.log(results);
             })
 
 
